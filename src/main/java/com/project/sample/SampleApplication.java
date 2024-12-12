005c8d6dddaef5e9d4d8c86436f27255fed45f91
Adesh <91891042+Rohitadesh@users.noreply.github.com>
2024-12-12 15:02:42 +0530

    Update SampleApplication.java

--- src/main/java/com/project/sample/SampleApplication.java
+++ src/main/java/com/project/sample/SampleApplication.java
@@ -2,12 +2,17 @@ package com.project.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/api")
 @SpringBootApplication
 public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
-
+	  @GetMapping("/")
+   	 public String health(){
+        return "Service is fine";
+    }
 }
